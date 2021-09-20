f1b673e4cec16b4f2e33c949f8c2149c1298805d
David Hong <phazex1@gmail.com>
2021-09-20 19:20:15 -0400

    Added a 3rd number implementation working

--- calc.js
+++ calc.js
@@ -13,9 +13,10 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	num3 = Number(tokens[3]);
 
 if (mathSymbol === "+") {
-	console.log (num1 + num2);
+	console.log (num1 + num2 + num3);
 }
 if (mathSymbol === '-') {
 	console.log (num1 - num2);
@@ -45,6 +46,7 @@ if (mathSymbol === '%') {
 console.log('mathSymbol', mathSymbol);
 console.log('num1', num1);
 console.log('num2', num2);
+console.log('num3', num3);
 
 	// This line closes the connection to the command line interface.
 	reader.close()
